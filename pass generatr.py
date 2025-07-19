19891892eca21206460c494cb22d52386669e452
Sanjeev Sarvesh <sanjusince200@gmail.com>
2025-07-19 12:48:28 +0530

    Add files via upload

---
+++ pass generatr.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+length = int(input("Enter your new password length: "))
+
+print("Your new password is:", generate_password(length))
